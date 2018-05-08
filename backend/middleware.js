dbfececdc3c596897685c68d7a5cc25b02eaabd7
Xiaoping Li <narci2008@gmail.com>
2018-05-08 14:40:07 -0700

    Add usersControllers and usersEndpoints to organize users endpoints

---
+++ backend/middleware.js
@@ -0,0 +1,41 @@
+const bcrypt = require('bcrypt');
+const users = require('./users/usersControllers');
+
+// Middleware to hash user's password
+const hashPassword = (req, res, next) => {
+  const {password } = req.body;
+  if (!password) {
+      res.json({ error: "Please provide password" });
+      return;
+  }
+
+  bcrypt
+      .hash(password, 11)
+      .then(hashedPW => {
+          req.password = hashedPW;
+          next();
+      })
+      .catch(error => {
+          res.json(error);
+      });
+};
+
+// Here is the middleware to validate if user logged in  
+const validateUser = (req, res, next) => {
+  const { username } = req.session;
+  if (!username) {
+      res.json({ error: "User is not logged in" });
+      return;
+  }
+
+  User.findOne({ username }, (err, user) => {
+      if (err) {
+          res.json(err);
+      } else if (!user) {
+          res.json({ error: "User does not exist!"});
+      } else {
+          req.user = user;
+          next();
+      }
+  });
+};
--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,9 @@ require('dotenv').config();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
-const db = require('/database/dbConfiguration.js');
 const session = require('express-session');
+
+const usersEndpoints = require('./users/usersEndpoints.js');
 
 const PORT = process.env.PORT || 5000;
 
@@ -35,6 +36,9 @@ server.use(
 
 
 //*************************** Middlewares *************************************************
+
+
+
 // Here is the middleware to validate if user logged in  
 const validateUser = (req, res, next) => {
     const { username } = req.session;
@@ -55,23 +59,7 @@ const validateUser = (req, res, next) => {
     });
 };
 
-const hashPassword = (req, res, next) => {
-    const {password } = req.body;
-    if (!password) {
-        res.json({ error: "Please provide password" });
-        return;
-    }
 
-    bcrypt
-        .hash(password, 11)
-        .then(hashedPW => {
-            req.password = hashedPW;
-            next();
-        })
-        .catch(error => {
-            res.json(error);
-        });
-};
 
 
 // ****************************************API Endpoints here***********************************
@@ -242,96 +230,42 @@ server.post('/sms/:mobile', (req, res) => {
 });
 
 // *******************Route controllers********************************************
+// const login = (req, res) => {
+//     const { username, password } = req.body;
+//     User.findOne({ username }, (err, user) => {
+//       if (err) {
+//         res.status(500).json({ error: 'Invalid Username/Password' });
+//         return;
+//       }
+//       if (user === null) {
+//         res.status(422).json({ error: 'No user with that username in our DB' });
+//         return;
+//       }
+//       user.checkPassword(password, (nonMatch, hashMatch) => {
+//         if (nonMatch !== null) {
+//           res.status(422).json({ error: 'passwords dont match' });
+//           return;
+//         }
+//         if (hashMatch) {
+//           req.session.username = username;
+//           req.user = user;
+//           res.json({ success: true });
+//         }
+//       });
+//     });
+// };
 
-const getUsers = (req, res) => {
-    
-    User.find({}, (err, users) => {
-        if (err) return res.send(err);
-        res.send(users);
-    });
-};
-
-const login = (req, res) => {
-    const { username, password } = req.body;
-    User.findOne({ username }, (err, user) => {
-      if (err) {
-        res.status(500).json({ error: 'Invalid Username/Password' });
-        return;
-      }
-      if (user === null) {
-        res.status(422).json({ error: 'No user with that username in our DB' });
-        return;
-      }
-      user.checkPassword(password, (nonMatch, hashMatch) => {
-        if (nonMatch !== null) {
-          res.status(422).json({ error: 'passwords dont match' });
-          return;
-        }
-        if (hashMatch) {
-          req.session.username = username;
-          req.user = user;
-          res.json({ success: true });
-        }
-      });
-    });
-};
-
-const signout = (req, res) => {
-    if (!req.session.username) {
-        res.json({ error: "User is not logged in!"});
-        return;
-    }
-    req.session.username = null;
-    res.json(req.session);
-};
-
-const updateUser = (req, res) => {
-    const id = req.params.id;
-    const userInfo = req.body;
-
-    User.findByIdAndUpdate(id, userInfo)
-        .then(user => {
-            res.status(200).json({ message: "User updated successfully"});
-        })
-        .catch(err => {
-            res.status(500).json({ message: "Server Error", err});
-        });
-};
-
-const deleteUser = (req, res) => {
-    const id = req.params.id;
-
-    User.findByIdAndRemove(id)
-        .then(user => {
-            res.status(200).json({ message: "User removed successfully"});
-        })
-        .catch(err => {
-            res.status(500).json({ message: "Server Error", err});
-        });
-};
-
-// ****************************************Users Routes*********************************************************
-
-server.post('/signup', (req, res) => {
-    const user = new User(req.body);
-
-    user.save()
-        .then(user => {
-            res.status(200).json(user);
-        })
-        .catch(err => {
-            res.status(500).json({ message: 'Server Error!'});
-        });
-});
-
-server.post('/signin', login);
-server.post('/signout', signout);
-server.get('/users', getUsers);
-server.put('/users/:id', updateUser);
-server.delete('/users/:id', deleteUser);
+// const signout = (req, res) => {
+//     if (!req.session.username) {
+//         res.json({ error: "User is not logged in!"});
+//         return;
+//     }
+//     req.session.username = null;
+//     res.json(req.session);
+// };
 
 
-
+server.use('', usersEndpoints);
 // ******************* MONGOOSE CONNECTION********************************
 
 server.listen(PORT, err => {
