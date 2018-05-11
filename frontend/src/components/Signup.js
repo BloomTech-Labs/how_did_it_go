b2d2979f7093978746fa8bf6c8b07c957c074034
Leela <narci2008@gmail.com>
2018-05-11 11:19:58 -0700

    Update Signup.js
    Update Signup.js

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -33,10 +33,11 @@ class SignUp extends Component {
       alert('Password and confirmed Password do not match!!! Please enter one more time.');
     } else {
       axios.post(ROOT_URL + 'signup', {username: this.state.username, password: this.state.password })
-        .then(function(response) {
-          console.log('hello world');
+        .then((response) => {
+          console.log('Sign Up successfully!');
         })
-        .catch(function(error) {
+        .catch((error) => {
+          alert('Failed to sign you up!');
           console.log(error);
         });
     }
